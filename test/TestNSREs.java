c231970cecc680500a13481e393b3d36d559393f
Benoit Sigoure <tsuna@stumbleupon.com>
2012-01-16 11:56:29 -0800

    Remove unused imports.
    Change-Id: Ib7da43caffb3dc9c2bca4283eb88854c56b204fb

--- test/TestNSREs.java
+++ test/TestNSREs.java
@@ -50,7 +50,6 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
