31cba3b8707bb389af89144cdfdd5b049166ed6a
Premal Shah <premal.j.shah@gmail.com>
2013-07-26 14:27:48 -0700

    added static method to compute variable size of data

--- src/HBaseRpc.java
+++ src/HBaseRpc.java
@@ -1047,4 +1047,17 @@ public abstract class HBaseRpc {
     return negate ? ~result : result;
   }
 
+  public static int getVIntSize(long i) {
+    if (i >= -112 && i <= 127) {
+      return 1;
+    }
+
+    if (i < 0) {
+      i ^= -1L; // take one's complement'
+    }
+    //find the number of bytes with non-leading zeros
+    int dataBits = Long.SIZE - Long.numberOfLeadingZeros(i);
+    // find the number of data bytes + length byte
+    return (dataBits + 7) / 8 + 1;
+  }
 }
