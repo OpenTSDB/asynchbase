53b3bf49d0c48bc5f112c7bc6696fe1bfe7cf698
Chris Larsen <clarsen@netflix.com>
2021-12-21 15:54:52 -0800

    Bump Logback version to patch CVE-2021-42550

--- third_party/logback/include.mk
+++ third_party/logback/include.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2012  The Async HBase Authors.  All rights reserved.
+# Copyright (C) 2011-2021  The Async HBase Authors.  All rights reserved.
 # This file is part of Async HBase.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -22,12 +22,13 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-http://central.maven.org/maven2/ch/qos/logback/logback-classic/1.0.13/logback-classic-1.0.13.jar
-LOGBACK_VERSION := 1.0.13
+
+https://repo1.maven.org/maven2/ch/qos/logback/logback-classic/1.2.9/logback-classic-1.2.9.jar
+LOGBACK_VERSION := 1.2.9
 
 LOGBACK_CLASSIC_VERSION := $(LOGBACK_VERSION)
 LOGBACK_CLASSIC := third_party/logback/logback-classic-$(LOGBACK_CLASSIC_VERSION).jar
-LOGBACK_CLASSIC_BASE_URL := http://central.maven.org/maven2/ch/qos/logback/logback-classic/$(LOGBACK_VERSION)
+LOGBACK_CLASSIC_BASE_URL := https://repo1.maven.org/maven2/ch/qos/logback/logback-classic/$(LOGBACK_VERSION)
 
 $(LOGBACK_CLASSIC): $(LOGBACK_CLASSIC).md5
 	set dummy "$(LOGBACK_CLASSIC_BASE_URL)" "$(LOGBACK_CLASSIC)"; shift; $(FETCH_DEPENDENCY)
@@ -35,7 +36,7 @@ $(LOGBACK_CLASSIC): $(LOGBACK_CLASSIC).md5
 
 LOGBACK_CORE_VERSION := $(LOGBACK_VERSION)
 LOGBACK_CORE := third_party/logback/logback-core-$(LOGBACK_CORE_VERSION).jar
-LOGBACK_CORE_BASE_URL := http://central.maven.org/maven2/ch/qos/logback/logback-core/$(LOGBACK_VERSION)
+LOGBACK_CORE_BASE_URL := https://repo1.maven.org/maven2/ch/qos/logback/logback-core/$(LOGBACK_VERSION)
 
 $(LOGBACK_CORE): $(LOGBACK_CORE).md5
 	set dummy "$(LOGBACK_CORE_BASE_URL)" "$(LOGBACK_CORE)"; shift; $(FETCH_DEPENDENCY)
